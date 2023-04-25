4b52f096ffa49a6c836553cf481aa7bcbd70af71
Matt surls <msurls@ChicagoBooth.edu>
2023-04-25 16:31:31 +0000

    First commit Up to here: https://chapters.firstdraft.com/chapters/843#re-using-the-moviedirector-method

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,19 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def actor
+    a_id = self.actor_id
+
+    matching_actors = Actor.where({ :id => a_id})
+
+    return matching_actors.at(0)
+  end
+
+  def movie
+    a_id = self.movie_id
+
+    matching_movies = Movies.where({ :id => a_id})
+
+    return matching_movies.at(0)
+  end
 end
